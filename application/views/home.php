20496bb7da223918904cb93f7f4900bdf5b2ea99
Nilotpal <nilotpal2007@gmail.com>
2014-02-20 22:22:30 +0530

    Design updates

--- application/views/home.php
+++ application/views/home.php
@@ -8,7 +8,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Process Designer</title>
-
+	<link href="/<?php echo $prefix; ?>/assets/css/jquery-ui.css" rel="stylesheet"/>
+	<link href="/<?php echo $prefix; ?>/assets/css/elements.css" rel="stylesheet"/>
     <!-- Bootstrap -->
     <link href="/<?php echo $prefix; ?>/assets/css/bootstrap.css" rel="stylesheet">
 	<!--<link href="/<?php echo $prefix; ?>/assets/css/bootstrap-theme.css" rel="stylesheet">-->
@@ -17,43 +18,62 @@
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
 	<link href="/<?php echo $prefix; ?>/assets/css/style.css" rel="stylesheet">
+	<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
   </head>
   <body>
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-12">
-				<div class="panel panel-default">
+				<div class="panel panel-main">
 					<div class="panel-heading">
-						<h3 class="panel-title"  style="display:inline;">Process designer</h3>
+						<h3 class="panel-title"  style="display:inline; font-weight: 600;">Process designer</h3>
 						<div class="dropdown pull-right" style="display:inline-block;">
-							<button class="btn btn-default btn-xs dropdown-toggle" type="button" data-toggle="dropdown">Dropdown <span class="caret"></span></button>
+							<button class="btn btn-link btn-xs dropdown-toggle" type="button" data-toggle="dropdown">Admin <span class="caret"></span></button>
 							<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
-								<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Action</a></li>
-								<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Another action</a></li>
-								<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Something else here</a></li>
+								<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Settings</a></li>
 								<li role="presentation" class="divider"></li>
-								<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Separated link</a></li>
+								<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Logout</a></li>
 							</ul>
 						</div>
 					</div>
 					<div class="panel-body">
-						<div class="row" style="margin-top: -15px; padding: 3px;background: #eaeaea;">
+						<div class="row" style="margin-top: -15px; padding: 3px;background: #dde2e8;">
 							<div class="col-lg-12" style="">
 								<div class="btn-toolbar" role="toolbar">
 									<div class="btn-group">
-										<button type="button" class="btn btn-default btn-sm">Left</button>
-										<button type="button" class="btn btn-default btn-sm">Middle</button>
-										<button type="button" class="btn btn-default btn-sm">Right</button>
+										<button type="button" class="btn btn-default btn-sm fa-lg"><span class="glyphicon glyphicon-floppy-save"></span></button>
+										<button type="button" class="btn btn-default btn-sm fa-lg"><i class="fa fa-folder-open"></i></button>
+										<button type="button" class="btn btn-default btn-sm fa-lg"><i class="glyphicon glyphicon-check"></i></button>
 									</div>
+									<!--<div class="dropdown" style="display:inline-block;">
+										<button class="btn btn-default btn-sm dropdown-toggle" type="button" data-toggle="dropdown">Insert <span class="caret"></span></button>
+										<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
+											<li role="presentation"><a role="menuitem" tabindex="-1" href="#" id="addShape">Process step</a></li>
+											<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Decision step</a></li>
+										</ul>
+									</div>-->
+									<button type="button" class="btn btn-default btn-sm fa-lg"><i class="fa fa-plus-circle" id="addShape"></i></button>
 								</div>
 							</div>
 						</div>
 						<div class="row">
-							<div class="col-lg-8">
-								
+							<div class="col-lg-9">
+								<div class="panel panel-tools">
+									<div class="panel-heading">Process</div>
+									<div class="panel-body">
+										<div class="fd process stage" id="process" style="height: 100%;">
+										
+										</div>
+									</div>
+								</div>
 							</div>
-							<div class="col-lg-4">
-								
+							<div class="col-lg-3">
+								<div class="panel panel-tools">
+									<div class="panel-heading">Tools</div>
+									<div class="panel-body">
+										
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
@@ -66,5 +86,8 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="/<?php echo $prefix; ?>/assets/js/bootstrap.min.js"></script>
+	<script src="/<?php echo $prefix; ?>/assets/js/jquery-ui.js"></script>
+	<script src="/<?php echo $prefix; ?>/assets/js/jquery.jsPlumb-1.5.5-min.js"></script>
+	<script src="/<?php echo $prefix; ?>/assets/js/app/app.js"></script>
   </body>
 </html>
